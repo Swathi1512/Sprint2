597ae2f14b9dc96bc3687b61bc0019232259645a
admin <admin@DESKTOP-JLCD7P7>
2020-11-29 11:05:38 +0530

    attendance controller

--- CITSMVC/Controllers/AttendanceController.cs
+++ CITSMVC/Controllers/AttendanceController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CITSMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,24 @@ namespace CITSMVC.Controllers
 {
     public class AttendanceController : Controller
     {
-        // GET: Attendance
+        // GET: EmpAttendance
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Attendance> attList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("EmpAttendance").Result;
+            attList = response.Content.ReadAsAsync<IEnumerable<Attendance>>().Result;
+            return View(attList);
+        }
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View(new Attendance());
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Attendance att)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("EmpAttendance", att).Result;
+            return RedirectToAction("Index");
         }
     }
 }
--- CITSMVC/Models/Attendance.cs
+++ CITSMVC/Models/Attendance.cs
@@ -7,5 +7,18 @@ namespace CITSMVC.Models
 {
     public class Attendance
     {
+        public int ID { get; set; }
+        public Nullable<int> EMP_CODE { get; set; }
+        public Nullable<int> Attend { get; set; }
+        public Nullable<System.DateTime> Date { get; set; }
+        public Nullable<System.TimeSpan> In_Time { get; set; }
+        public Nullable<System.TimeSpan> Out_Time { get; set; }
+        public string Permission { get; set; }
+        public string IsLate { get; set; }
+        public string Half { get; set; }
+        public string Notes { get; set; }
+        public string App_User { get; set; }
+        public Nullable<System.DateTime> Creation_Date { get; set; }
+        public Nullable<System.DateTime> Update_Date { get; set; }
     }
 }
